67f0feaafb592fb9d6be9f093759dff9ba7df372
nafeyuldin1 <lasthwwy@gmail.com>
2025-01-08 09:24:29 -0800

    files updates

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Gallery = () => {
   return (
-    <div className='grid grid-cols-2 md:grid-cols-3 gap-4 m-4'>
+    <div className='grid grid-cols-1 md:grid-cols-3 gap-4 m-4'>
         {GalleryInfo.map((item, index) => (
             <div  key={index} className='md:h-[400px]'>
                <Image className='h-full  object-cover w-full' width={1000} height={1000} src={item.url} alt="image" />
